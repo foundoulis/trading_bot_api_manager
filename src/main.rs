ed28e444aebe694a6b44d5ddd8b4550ad26b0264
Derek Foundoulis <derek@foundoulis.io>
2021-02-01 21:29:34 -0700

    Can access market data.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+
+pub fn ping_api(route: &str) -> reqwest::Result<reqwest::blocking::Response> {
+    let mut cb_url = String::from("https://api.pro.coinbase.com/");
+    cb_url.push_str(route);
+    reqwest::blocking::get(&cb_url)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = ping_api(&String::from("currencies")).unwrap();
+    println!("{:#?}", resp.text());
+    Ok(())
 }
