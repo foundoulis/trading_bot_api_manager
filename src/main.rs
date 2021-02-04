2f6e52a66b0b3d08578b6475a77be0afad6a3f9d
Derek Foundoulis <derek@foundoulis.io>
2021-02-03 22:51:05 -0700

    Added simple coinbase client that gets the time.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 use reqwest;
 
-pub fn ping_api(route: &str) -> reqwest::Result<reqwest::blocking::Response> {
-    let mut cb_url = String::from("https://api.pro.coinbase.com/");
-    cb_url.push_str(route);
-    reqwest::blocking::get(&cb_url)
+pub struct CoinbaseClient {
+    url: String,
+    cli: reqwest::blocking::Client
+}
+
+impl CoinbaseClient {
+    pub fn new() -> CoinbaseClient {
+        CoinbaseClient {
+            url: String::from("https://api.pro.coinbase.com/"),
+            cli: reqwest::blocking::Client::new()
+        }
+    }
+
+    pub fn ping_api(&self, route: &str) -> reqwest::Result<reqwest::blocking::Response> {
+        self.cli.get(&format!("{}{}", self.url, route))
+            .header("User-Agent", "foundoulis/1.0")
+            .send()
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = ping_api(&String::from("currencies")).unwrap();
+    let client = CoinbaseClient::new();
+    let resp = client.ping_api("time").unwrap();
     println!("{:#?}", resp.text());
     Ok(())
 }
